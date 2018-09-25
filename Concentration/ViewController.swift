914e172559d55098442afcb7c1e605aca94307af
Denver Stove <dtstove@gmail.com>
2018-09-25 23:02:34 +1000

    Finished end the first lecture, buttons and flips done!

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "🚀", on: sender)
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
     
+    @IBOutlet weak var flipCountLabel: UITextField!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🤯", "🤫", "😎", "😈", "🐏", "🍵", "🥟", "🎺", "🏩", "⌛️", "😎", "🍵", "🥟", "🤯", "🏩", "🤫", "🐏", "⌛️", "🎺", "😈"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+    }
+
     func flipCard(withEmoji emoji: String, on button: UIButton) {
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5212053061, blue: 1, alpha: 1)
         } else {
             button.setTitle(emoji, for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.9768045545, blue: 0.6977539062, alpha: 1)
