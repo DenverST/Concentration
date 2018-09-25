a92000a7d8a699473afef9443682b4dd5148e0cc
Denver Stove <dtstove@gmail.com>
2018-09-25 22:01:47 +1000

    Finished coding the flip of the first button

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
 
     
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🚀", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.9768045545, blue: 0.6977539062, alpha: 1)
+        }
     }
     
 }
