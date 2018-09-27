cf35525a47c4034f1936a203363fa673a2cd1c07
Denver Stove <dtstove@gmail.com>
2018-09-27 15:34:04 +1000

    Added an extension in ViewController

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -63,27 +63,22 @@ class ViewController: UIViewController {
         
     private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int (arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
             }
-        
         return emoji[card.identifier] ?? "?"
-//        if emoji[card.identifier] != nil {
-//            return emoji[card.identifier]!
-//        } else {
-//            return "?"
-//        }
     }
 }
 
-//    func flipCard(withEmoji emoji: String, on button: UIButton) {
-//        if button.currentTitle == emoji {
-//            button.setTitle("", for: UIControl.State.normal)
-//            button.backgroundColor = #colorLiteral(red: 1, green: 0.5212053061, blue: 1, alpha: 1)
-//        } else {
-//            button.setTitle(emoji, for: UIControl.State.normal)
-//            button.backgroundColor = #colorLiteral(red: 1, green: 0.9768045545, blue: 0.6977539062, alpha: 1)
-//        }
-//    }
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
 
 
