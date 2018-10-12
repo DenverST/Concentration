07775c88dc0275d2a3e8e4a6c94ae709ee1b0335
Denver Stove <dtstove@gmail.com>
2018-10-12 12:17:33 +1100

    Did all types of view, also made changes so that it operates in different ways on different devices

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ConcentrationThemeChooserViewController: UIViewController {
+class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
 
 
     let themes = [
@@ -17,12 +17,47 @@ class ConcentrationThemeChooserViewController: UIViewController {
         "Tricky":"⌛️🤞✌️⏳⚒🛠🕐🕑◾️◼️🔡🔠",
     ]
     
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        if let cvc = secondaryViewController as? ConcentrationViewController {
+            if cvc.theme == nil {
+                return true
+            }
+        }
+        return false
+    }
+    
+    @IBAction func ChangeTheme(_ sender: Any) {
+        if let cvc = splitViewDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+        } else if let cvc = lastSeguedToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+        performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
+    
+    private var lastSeguedToConcentrationViewController: ConcentrationViewController?
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
         if segue.identifier == "Choose Theme" {
             if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
                 if let cvc = segue.destination as? ConcentrationViewController {
                     cvc.theme = theme
+                    lastSeguedToConcentrationViewController = cvc
                 }
             }
         }
